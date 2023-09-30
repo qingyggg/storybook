14dc5bd8e273cb78169ff39db53cf61d98eaf954
alice <alice786mols@gmail.com>
2023-09-30 19:10:11 +0800

    1.get ServerSideProps bug

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -3,47 +3,64 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import { Link } from '@mui/material';
 import { useRouter } from 'next/router';
-import { showProfileApi } from '../../api/user';
-import { editProfileI } from '../../api/user/reqTypes';
+import { showProfileApi } from '@/api/user';
+import { editProfileI } from '@/api/user/reqTypes';
 import { showProfileI } from '../../api/user/resTypes';
 import { useRequest } from '../../hooks/useRequest';
-import { idTransform } from '../../util/common';
+import { dataConsumer, idTransform } from 'util/common';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 
-export default function Profile() {
-  const router = useRouter();
-  const { id } = router.query;
-  const [Profile, setProfile] = useState<editProfileI>(nullProfile);
-  const showProfile = useRequest<showProfileI>(
-    showProfileApi(idTransform(id)),
-    (res) => setProfile(res!),
-    () => void 0,
-    true,
-  );
-  useEffect(() => {
-    if (idTransform(id) === -1) {
-      // return "cnm ,react!!!"
-      return;
-    }
-    const api = async () => {
-      //get original profile content
-      (await showProfile)();
-    };
-    api();
-  }, [id]);
-  //complete before 4pm
+//bug logic
+export const getServerSideProps = (async (context) => {
+  const uid = context.params?.id;
+  //error log:showProfile api is not defined
+  //error log:showProfile api is not defined
+  //error log:showProfile api is not defined
+  //error log:showProfile api is not defined
+  const { data } = await showProfileApi(idTransform(uid))();
+  console.log(data);
+  const profile = dataConsumer(data);
+  return { props: { profile: nullProfile } };
+}) satisfies GetServerSideProps<{
+  profile: editProfileI;
+}>;
+
+export default function Profile({
+  profile,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // const router = useRouter();
+  // const { id } = router.query;
+  // const [profile, setProfile] = useState<editProfileI>(nullProfile);
+  // const showProfile = useRequest<showProfileI>(
+  //   showProfileApi(idTransform(id)),
+  //   (res) => setProfile(res!),
+  //   () => void 0,
+  //   true,
+  // );
+  // useEffect(() => {
+  //   if (idTransform(id) === -1) {
+  //     // return "cnm ,react!!!"
+  //     return;
+  //   }
+  //   const api = async () => {
+  //     //get original profile content
+  //     (await showProfile)();
+  //   };
+  //   api();
+  // }, [id]);
   return (
     <div className='w-full flex flex-col items-center mt-5'>
       <h1 className='text-5xl'>user profile</h1>
       <div className='flex flex-col text-2xl'>
-        <p>name:{Profile.Name}</p>
-        <p>age:{Profile.Age}</p>
-        <p>Description:{Profile.Description}</p>
+        <p>name:{profile.Name}</p>
+        <p>age:{profile.Age}</p>
+        <p>Description:{profile.Description}</p>
         <div className='flex flex-row'>
-          <Link href={Profile.Github} underline='none'>
+          <Link href={profile.Github} underline='none'>
             <GitHubIcon fontSize='large' />
           </Link>
           <div className='mx-8'></div>
-          <Link href={Profile.Twitter} underline='none'>
+          <Link href={profile.Twitter} underline='none'>
             <TwitterIcon fontSize='large'></TwitterIcon>
           </Link>
         </div>
